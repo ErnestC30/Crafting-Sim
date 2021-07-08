77f948dca35ddd9dfd352fec2ceb6babec7de157
ErnestC30 <chow_ernest06@hotmail.com>
2021-07-08 18:27:02 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, redirect, url_for, render_template
+
+#Use bootstrap to handle CSS/JS part?, SQLAlchemy for database
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template("home.html")
+
+@app.route('/crafting')
+def crafting():
+    return render_template("crafting.html")
+
+@app.route('/crafts')
+def crafts():
+    return render_template("crafts.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
